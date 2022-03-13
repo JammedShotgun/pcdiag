a659afeb1176514cddf03b7f8fd6166c83f47b41
Noël <61549754+JammedShotgun@users.noreply.github.com>
2022-03-13 11:42:36 +0700

    Add files via upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,100 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+    let newItem = '';
+	
+    let todoList = [{text: 'Monitor tidak menyala', status: false},
+                    {text: 'Komputer hidup tapi tidak boot', status: false},
+                    {text: 'Komputer booting ke safe mode', status: false},
+					{text: 'RAM tidak terdeteksi', status: false},
+                    {text: 'Komputer sering hang', status: false},
+                    {text: 'Tampilan layar bergaris', status: false},
+					{text: 'Muncul tulisan "Operating system not found"', status: false},
+                    {text: 'Hard Disk bad sector', status: false},
+                    {text: 'Muncul tulisan "Memory test fail"', status: false},
+					{text: 'Muncul tulisan "Hard disk error/failure"', status: false},
+                    {text: 'Tidak dapat masuk ke BIOS', status: false},
+                    {text: 'Muncul tulisan "Memory Management Error"', status: false},
+					{text: 'Muncul tulisan "NTFS FILE SYSTEM"', status: false},
+					{text: 'Missing dll32', status: false},];
+	
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
+	}
+	
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
+    }
+
+</script>
+
+
+
+<svelte:head>
+	<title>Pc Diagnostic</title>
+</svelte:head>
+
+<section>
+	<h1>
+		PC Diagnostic
+	</h1>
+
+	<h2>
+		Pilih beberapa gejala yang dialami
+	</h2>
+</section>
+<div class="cum">
+{#each todoList as item, index}
+<input bind:checked={item.status} type="checkbox">
+<span class:checked={item.status}>{item.text}</span>
+<br/>
+<br/>
+{/each} 
+</div>
+<button type="submit">
+	Submit
+</button>
+<style>
+	.cum {
+		margin-left: auto;
+    	margin-right: auto;
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+	.checked {
+        text-decoration: line-through;
+    }
+	button {
+		width: fit-content;
+		margin-left: auto;
+    	margin-right: auto;
+	}
+</style>
---
+++ src/routes/resultFinal.svelte
@@ -0,0 +1,71 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+
+</script>
+
+
+
+<svelte:head>
+	<title>Results</title>
+</svelte:head>
+
+<section>
+	<h1>
+		PC Diagnostic
+	</h1>
+
+	<h2>
+		Hasil klasifikasi kerusakan:
+	</h2>
+
+	<h3>
+		HARDWARE
+	</h3>
+	
+	<br/>
+Berdasarkan gejala yang telah anda berikan hasil analisis sistem menunjukkan bahwa monitor anda bermasalah. Bila monitor anda tidak dapat menyala, coba pastikan kabel yang terhubung dari monitor ke CPU anda tidak bermasalah. Bila masih tidak dapat menyala, disarankan untuk melalukan pembaharuan driver VGA anda.
+<br/>
+<br/>
+</section>
+<button type="submit">
+	Kembali
+</button>
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+	.checked {
+        font-weight: bold;
+    }
+    button {
+		width: fit-content;
+		margin-left: auto;
+    	margin-right: auto;
+	}
+</style>
